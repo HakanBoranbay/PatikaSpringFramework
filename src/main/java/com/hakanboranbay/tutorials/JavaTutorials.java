3377967eabf87e0e70f3657d775c29983bc7d7f8
Hakan Boranbay <boranbayhakan@gmail.com>
2023-01-18 21:43:58 +0300

    Hatırlatma

---
+++ src/main/java/com/hakanboranbay/tutorials/JavaTutorials.java
@@ -0,0 +1,62 @@
+package com.hakanboranbay.tutorials;
+
+import java.util.Arrays;
+
+public class JavaTutorials {
+
+    public static void main(String[] args) {
+        // Arrays.asList();
+        // this.
+        // super()
+        // break, continue, return
+        // optional
+        // cast
+        // primitive type
+        // wrapper class
+        // Random
+        // Math
+        // %s %d %f ==> printf()
+        // final
+        // static
+        // decoder encoder
+        // instanceof
+        // access identifiers public private protected
+        // String
+        // StringBuilder
+        // StringBuffer
+        // StringTokenizer
+        // Enum
+        // Multithread
+        // Collections
+        // Iterator
+
+        // S.O.L.I.D.
+        // Clean Code
+        // Open-Closed
+        // DRY
+        // MONAD
+        // BigO Notation
+        // KISS
+
+        // UML - + _
+        // POJO
+        // Bean
+        // Serialization
+        // IO
+        // JAR WAR
+        // Dağıtık Programlama
+        // server
+
+        // constructor
+        // exception
+        // error
+
+        // Java 8 : stream, map, filter, sort
+        // Optional
+        // Lambda Expression
+        // Method references ::
+
+        // D.P. Design Patterns
+    }
+
+}
