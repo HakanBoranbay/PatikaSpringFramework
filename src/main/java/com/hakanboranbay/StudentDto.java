cd8940b77e380048740cc87ba9256e3809431990
Hakan Boranbay <boranbayhakan@gmail.com>
2023-01-19 00:25:04 +0300

    StudentDto

---
+++ src/main/java/com/hakanboranbay/StudentDto.java
@@ -0,0 +1,19 @@
+package com.hakanboranbay;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Data Transfer Object = DTO
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentDto {
+
+    private Long studentId;
+    private String studentName;
+    private String studentSurname;
+
+}
